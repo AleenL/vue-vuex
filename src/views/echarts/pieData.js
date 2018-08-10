e8bf3f8014b29c1b9be97fb416060e868b7d9a6a
784757555@qq.com <784757555@qq.com>
2018-08-10 18:38:45 +0800

    first commit

---
+++ src/views/echarts/pieData.js
@@ -0,0 +1,9 @@
+/*
+this is my stupid code 
+and only me can understand
+I can hear rain knocking the window
+The Moon hide in this city 
+Wish you have a sweet dream
+
+created by Allen in 2018/05/29
+*/
---
+++ src/views/leftSideBox/index.vue
@@ -0,0 +1,13 @@
+<!--created by allen on 2018/05/29-->
+
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
